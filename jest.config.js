ed3ca8422deb41ea91413ead3c334ce186201e40
Laura <laura.heddleston@gmail.com>
2020-04-16 00:06:52 -0700

    Remove unused dependencies

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  clearMocks: true,
-  coverageDirectory: 'coverage',
-  setupFilesAfterEnv: ['jest-enzyme'],
-  testEnvironment: 'enzyme',
-  testEnvironmentOptions: {
-    enzymeAdapter: 'react16',
-  },
-};
